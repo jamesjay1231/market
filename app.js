9f3d1475c40f06827d755dc16dc504ff0d163c8d
jay <jaeyoung6720@naver.com>
2024-08-06 13:15:58 +0900

    로그린 버튼 수정

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-function showForm(formId) {
-  // 모든 폼을 숨기기
-  const forms = document.querySelectorAll(".form");
-  forms.forEach((form) => {
-    form.style.display = "none";
-  });
+// function showForm(formId) {
+//   // 모든 폼을 숨기기
+//   const forms = document.querySelector(".form");
+//   forms.forEach((form) => {
+//     form.style.display = "none";
+//   });
 
-  // 선택한 폼만 보이기
-  document.getElementById(formId).style.display = "block";
-}
+//   // 선택한 폼만 보이기
+//   document.getElementById(formId).style.display = "block";
+// }
 
-// 기본적으로 첫 번째 폼을 보이게 하기
-document.addEventListener("DOMContentLoaded", () => {
-  showForm("buyer-login");
-});
+// // 기본적으로 첫 번째 폼을 보이게 하기
+// document.addEventListener("DOMContentLoaded", () => {
+//   showForm("buyer-login");
+// });
